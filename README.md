6ac41e78f9dcf4d5223218400db1614a03932d31
Louis-Guillaume MORAND <lgm@lgmorand.com>
2021-01-13 12:22:36 +0100

    test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# book-github-actions-repo
-Repository d'exemple
+# Sample repository
+
+This repository is just a sample repository to learn on how to use Github Actions
+
+Source code is a modified version of Codepen bear from Ryan Joseph [https://github.com/ryan-joseph-dev](https://github.com/ryan-joseph-dev)
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp"), pug = require('gulp-pug'), runSequence = require('gulp4-run-sequence');;
+
+gulp.task('pugToHtml', () => {
+   return gulp.src('src/*.pug')
+   .pipe(pug({
+      pretty: true
+   }))
+   .pipe(gulp.dest('dest'));
+ });
+
+
+gulp.task('copy', () => {
+   return gulp.src([
+     "src/styles/*.*",
+   ], {
+     dot: true,
+   })
+   .pipe(gulp.dest('dest'));
+ });
+
+gulp.task('build', done => {
+   runSequence(
+     ['pugToHtml'],
+     ['copy'],
+     done
+   );
+ });
